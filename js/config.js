8ecdec2c151607232618d16de8d0b7f2c6541831
bellevueyouthdebate <outreach@bellevueyouthdebate.org>
2026-07-05 07:38:42 -0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,11 @@
+/**
+ * config.js
+ * The ONE thing you must edit before this app will work.
+ *
+ * After deploying the Apps Script backend as a Web App (see README step 3),
+ * paste the Web App URL below. It looks like:
+ *   https://script.google.com/macros/s/AKfycb.../exec
+ */
+window.BYD_CONFIG = {
+  API_URL: 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE'
+};
